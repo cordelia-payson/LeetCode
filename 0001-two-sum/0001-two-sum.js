ceddba7f13499e2f736a5d15857b3b64851cca61
Cordelia Payson <cordelia.payson@gmail.com>
2023-01-24 09:52:06 -0500

    Time: 154 ms (32.95%), Space: 42.1 MB (81.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,6 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // var array for result
-    
-    // start looping through nums
-      // i is current num
-        // loop through nums starting at num + 1
-          // add num at i and num at j
-            // if target return [i,j]
-    
     for (var i = 0; i < nums.length; i++) {
         for (var j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
