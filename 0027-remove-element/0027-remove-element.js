4356a8825c709015732b312a0d53e0869a755a4d
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-10 10:27:45 -0500

    Time: 69 ms (49.26%), Space: 42.6 MB (8.14%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,17 +4,34 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+ // two methods to compare speed and memory usage 
   
   if (nums.length === 0) {
     return 0;
   }
   
-  for (var i = -1; i < nums.length; i++) {
-    let index = i + 1;
-    while (nums[index] === val) {
-      nums.splice(index, 1);
+//   for (var i = -1; i < nums.length; i++) {
+//     let index = i + 1;
+//     while (nums[index] === val) {
+//       nums.splice(index, 1);
+//     }
+//    }
+//   return nums.length;
+  
+  
+  nums.sort((a,b) => a - b);
+  
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      let index = i + 1;
+      let count = 1;
+      while (nums[index] === val) {
+        count++;
+        index++;
+      }
+      nums.splice(i, count);
+      return nums.length;
     }
-   }
-  return nums.length;
+  }
 };
 
