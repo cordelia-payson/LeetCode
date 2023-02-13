281f3216acafc6339efb674399cd109e62eed435
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-13 10:56:15 -0500

    Time: 63 ms (62.67%), Space: 42.1 MB (30.23%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -0,0 +1,57 @@
+
+var MyQueue = function() {
+  this.stack1 = [];
+  this.stack2 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+  this.stack1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+  if (this.stack2.length) {
+    return this.stack2.pop();
+  }
+  while (this.stack1.length) {
+    this.stack2.push(this.stack1.pop());
+  }
+ return this.stack2.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+  if (this.stack2.length) {
+   return this.stack2.slice(-1);
+  }
+  return this.stack1[0];
+  
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+  if (!this.stack1.length && !this.stack2.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
