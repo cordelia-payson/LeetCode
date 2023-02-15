2cf0af4db412d609cd8f45ebf7d395a0702d506b
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-15 10:54:06 -0500

    Time: 96 ms (77.80%), Space: 50.4 MB (77.33%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -8,11 +8,6 @@ var nearestValidPoint = function(x, y, points) {
   let index = -1;
   let dist = Number.MAX_VALUE;
   
-  var distance = function(a, b) {
-    return Math.abs(x - a) + Math.abs(y - b);
-    return result;
-  };
-  
   for (var i = 0; i < points.length; i++) {
     if (points[i][0] === x || points[i][1] === y) {
       let currDist = Math.abs(x - points[i][0]) + Math.abs(y - points[i][1]);
